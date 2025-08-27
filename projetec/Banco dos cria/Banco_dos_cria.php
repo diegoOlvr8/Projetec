434e97fcfbad783bb7db425ccc67893eabfe7b22
davicd57 <davibr497@gmail.com>
2025-08-27 11:35:17 -0300

    Banco_dos_cria.php

--- projetec/Banco dos cria/Banco_dos_cria.php
+++ projetec/Banco dos cria/Banco_dos_cria.php
@@ -13,7 +13,7 @@
 
     $sql="INSERT INTO ambiente_compartilhado(Codigo_de_acesso) VALUES ('$Codigo_de_acesso')";
 
-        quarto={"Arrumar a cama","Trocar a roupa de cama","Guardar as roupas limpas","Dobrar as roupas",
+    quarto={"Arrumar a cama","Trocar a roupa de cama","Guardar as roupas limpas","Dobrar as roupas",
             "Separar as roupas sujas","Limpar os móveis","Organizar armário","Varrer o chão",
             "Passar pano no chão","Organizar gavetas","Limpar espelho","Recolher o lixo do quarto"
             }
@@ -28,5 +28,6 @@
 </body>
 
 </html>
+
 
 
