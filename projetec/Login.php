5b7bf7d3a377d596af0d7ee093ae385a5358796f
davicd57 <davibr497@gmail.com>
2025-09-24 11:38:25 -0300

    Login.php

--- projetec/Login.php
+++ projetec/Login.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="Login.php" method="post">
-    <img src="" alt="">
-    <input type="text" name="a">
-    <input type="password" name="b">
-    <input type="submit" value="Enviar">
-</form>
-
 <?php
 $user=$_POST['a'];
 echo "$user";
@@ -27,5 +12,4 @@ else{echo "deu erro";}
 
 ?>
 
-</body>
-</html>
+
