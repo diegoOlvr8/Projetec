5cea86bf136465ae4703cb4ee01c6dd2c6a7e6fa
davicd57 <davibr497@gmail.com>
2025-10-01 12:08:09 -0300

    Login.php

--- projetec/Login.php
+++ projetec/Login.php
@@ -1,58 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="Login.php" method="post">
-    <img src="" alt="">
-    <input type="text" name="usuario"><br>
-    <input type="password" name="senha"> <br> <br>
-    <input type="submit" name="Entrar" value="Entrar">
-    <input type="submit" name="Criar" value="Criar">
-</form>
-
 <?php
-session_start();
-$usuario=$_POST['usuario'];
-$senha=$_POST['senha'];
+$user=$_POST['a'];
+$senha=$_POST['b'];
 
-if (!isset($_SESSION['usuarios'])){
-    $_SESSION['usuarios']=[];
-    $_SESSION['senhas']=[];
+$usuarios=['Davi','Diego'];
+$senhas=['12345678','senha123'];
+
+$posicao=array_search($user,$usuarios);
+
+function Criar(){
+array_push($usuarios,$user);
+array_push($senhas,$senha);
 }
 
-$usuarios_registrados=&$_SESSION['usuarios'];
-$senhas_registradas=&$_SESSION['senhas'];
-
-
-function Entrar($usuario,$senha,&$usuarios_registrados,&$senhas_registradas){
-    $posicao=array_search($usuario,$usuarios_registrados);
-
-    if ($senha===$senhas_registradas[$posicao]){
-        echo "Login feito com sucesso";
-        header('Location:https://diegoolvr8.github.io/Projetec/projetec/Pagina_inicial.html');
-    }
-    else{
-        echo "Senha Incorreta";
-    }
+function Entrar(){  
+if (strlen($senha)<8){
+    echo "Sua senha deve ter no minimo 8 digitos";
 }
+
+
+if ($senha===$senhas[$posicao]){
+    echo "Login feito com sucesso";
+    header('Location:https://diegoolvr8.github.io/Projetec/projetec/Pagina_inicial.html');   
+}
+else{
+    echo "Senha indisponivel";
+}
+}
+
 if (isset($_POST['Entrar'])){
-    Entrar($usuario,$senha,$usuarios_registrados,$senhas_registradas);
-}
-
-function Criar($usuario,$senha,&$usuarios_registrados,&$senhas_registradas){
-    array_push($usuarios_registrados,$usuario);
-    array_push($senhas_registradas,$senha);
-
+    Entrar();
 }
 
 if (isset($_POST['Criar'])){
-    Criar($usuario,$senha,$usuarios_registrados,$senhas_registradas);
+    Criar();
 }
-
 ?>
-</body>
-</html>
