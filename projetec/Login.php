63137a1831bd40d8d777bf20bbe56d6cb25634e1
davicd57 <davibr497@gmail.com>
2025-10-01 12:00:37 -0300

    Login.php

--- projetec/Login.php
+++ projetec/Login.php
@@ -8,49 +8,50 @@
 <body>
     <form action="Login.php" method="post">
     <img src="" alt="">
-    <input type="text" name="a"><br>
-    <input type="password" name="b">
+    <input type="text" name="usuario"><br>
+    <input type="password" name="senha"> <br> <br>
     <input type="submit" name="Entrar" value="Entrar">
     <input type="submit" name="Criar" value="Criar">
 </form>
 
 <?php
-$user=$_POST['a'];
-$senha=$_POST['b'];
+session_start();
+$usuario=$_POST['usuario'];
+$senha=$_POST['senha'];
 
-$usuarios=['Davi','Diego'];
-$senhas=['12345678','senha123'];
-
-$posicao=array_search($user,$usuarios);
-
-function Criar(){
-array_push($usuarios,$user);
-array_push($senhas,$senha);
+if (!isset($_SESSION['usuarios'])){
+    $_SESSION['usuarios']=[];
+    $_SESSION['senhas']=[];
 }
 
-function Entrar(){  
-if (strlen($senha)<8){
-    echo "Sua senha deve ter no minimo 8 digitos";
-}
+$usuarios_registrados=&$_SESSION['usuarios'];
+$senhas_registradas=&$_SESSION['senhas'];
 
 
-if ($senha===$senhas[$posicao]){
-    echo "Login feito com sucesso";
-    header('Location:https://diegoolvr8.github.io/Projetec/projetec/Pagina_inicial.html');
-}
-else{
-    echo "Senha indisponivel";
-}
-}
+function Entrar($usuario,$senha,&$usuarios_registrados,&$senhas_registradas){
+    $posicao=array_search($usuario,$usuarios_registrados);
 
+    if ($senha===$senhas_registradas[$posicao]){
+        echo "Login feito com sucesso";
+        header('Location:https://diegoolvr8.github.io/Projetec/projetec/Pagina_inicial.html');
+    }
+    else{
+        echo "Senha Incorreta";
+    }
+}
 if (isset($_POST['Entrar'])){
-    Entrar();
+    Entrar($usuario,$senha,$usuarios_registrados,$senhas_registradas);
+}
+
+function Criar($usuario,$senha,&$usuarios_registrados,&$senhas_registradas){
+    array_push($usuarios_registrados,$usuario);
+    array_push($senhas_registradas,$senha);
+
 }
 
 if (isset($_POST['Criar'])){
-    Criar();
+    Criar($usuario,$senha,$usuarios_registrados,$senhas_registradas);
 }
-
 
 ?>
 </body>
