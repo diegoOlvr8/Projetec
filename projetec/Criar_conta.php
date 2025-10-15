75f9e2075835cc272dcc822698d568c1fccf14a0
davicd57 <davibr497@gmail.com>
2025-10-15 12:07:42 -0300

    Criar_conta.php

--- projetec/Criar_conta.php
+++ projetec/Criar_conta.php
@@ -7,4 +7,9 @@ if ($confirmacao===$criar_senha){
     echo "senhas iguais";
     header('Location:https://diegoolvr8.github.io/Projetec/projetec/Login.html');
 }
+
+if ($confirmacao!==$criar_senha){
+    echo "senhas diferentes";
+    header('Location:https://diegoolvr8.github.io/Projetec/projetec/Criar_conta.html');
+}
 ?>
