2235769cf9375e60ff138333d69b9cb3e4c1bcbc
cremozin <165583702+cremozin@users.noreply.github.com>
2025-11-28 03:17:23 -0300

    Add files via upload

---
+++ projetec/Login.php
@@ -0,0 +1,32 @@
+<?php
+if (isset($_POST['Entrar'])) {
+    $email = $_POST['usuarios'];
+    $senha = $_POST['senhas'];
+
+    
+    $arquivo = 'usuarios.txt';
+
+    if (file_exists($arquivo)) {
+        $usuarios = file($arquivo, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+        $loginValido = false;
+
+        foreach ($usuarios as $usuario) {
+            list($emailSalvo, $senhaSalva) = explode(";", $usuario);
+            if (trim($email) === trim($emailSalvo) && trim($senha) === trim($senhaSalva)) {
+                $loginValido = true;
+                break;
+            }
+        }
+
+        if ($loginValido) {
+            header('Location:https://diegoolvr8.github.io/Projetec/projetec/home.html');
+            exit();
+        } else {
+            echo "<script>alert('Email ou senha incorretos!'); window.history.back();</script>";
+        }
+    } else {
+        echo "<script>alert('Arquivo de usuários não encontrado!'); window.history.back();</script>";
+    }
+}
+?>
